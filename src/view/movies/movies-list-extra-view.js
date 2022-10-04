cc66c2309740e174af29caf5e1a28f732493ac11
Sergey Sytsevich <97549732+sytsevichs@users.noreply.github.com>
2022-10-05 00:03:09 +0300

    Возвращение удаленного файла

---
+++ src/view/movies/movies-list-extra-view.js
@@ -0,0 +1,11 @@
+import AbstactView from '../../framework/view/abstract-view.js';
+
+const createMoviesListExtraView = () => (`
+<section class="films-list films-list--extra">
+</section>`
+);
+export default class MoviesListExtraView extends AbstactView {
+  get template() {
+    return createMoviesListExtraView();
+  }
+}
